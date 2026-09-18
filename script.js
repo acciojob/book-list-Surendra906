1812515086bdf4b42bb95571dea44b8e1ea4d2bc
Surendra906 <surendramode945@gmail.com>
2026-09-18 13:35:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+document.getElementById('book-form').addEventListener('submit',function(e){
+	e.preventDefault();
+
+	const title=document.getElementById('title').value;
+	const author=document.getElementById('author').value;
+	const isbn=document.getElementById('isbn').value;
+	const bookList=document.getElementById('book-list');
+	const row=document.creatElement('tr');
+	row.innerHTML=`
+	<td>${title}</td>
+	<td>${author}</td>
+	<td>${isbn}</td>
+	<td><button class="delete">clear</button></td>
+	`;
+
+	bookList.appendChild(row);
+	document.getElementById('title').value='';
+	document.getElementById('author').value='';
+	document.getElementById('isbn').value='';
+});
+document.getElementById('book-list').addEventlistener('click',function(e){
+	if(e.target.classList.contains('delete')){
+		e.taarget.parentElement.parentElement.remove();
+	}
+});
